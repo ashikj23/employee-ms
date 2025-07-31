60bc13cb0ca67b1ca17a31b3e208e785c950406e
ashikj23 <ashikj2003@gmail.com>
2025-07-31 14:57:46 +0530

    intial commit

---
+++ springboot-employee-management-system-api-master/src/main/java/com/employeems/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.employeems.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*");
+            }
+        };
+    }
+   
+}
